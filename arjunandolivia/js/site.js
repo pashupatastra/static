1a6894809f80b707d469617ef5bf8db8d83aea73
gholdband <gholdband@gmail.com>
2017-04-02 19:44:05 +0100

    fixed form conflict

--- arjunandolivia/js/site.js
+++ arjunandolivia/js/site.js
@@ -119,15 +119,15 @@ var iframe = document.querySelector('iframe');
 		$("#topknot").trigger('play');
 	});
 
-// rsvp form submit
+// rsvp 1 form submit
 
 $(function() {
 
 	// Get the form.
-	var form = $('#ajax-rsvp');
+	var form = $('#ajax-rsvp1');
 
 	// Get the messages div.
-	var formMessages = $('#form-messages');
+	var formMessages1 = $('#form-messages1');
 
 	// Set up an event listener for the contact form.
 	$(form).submit(function(e) {
@@ -145,11 +145,11 @@ $(function() {
 		})
 		.done(function(response) {
 			// Make sure that the formMessages div has the 'success' class.
-			$(formMessages).removeClass('error');
-			$(formMessages).addClass('success');
+			$(formMessages1).removeClass('error');
+			$(formMessages1).addClass('success');
 
 			// Set the message text.
-			$(formMessages).text(response);
+			$(formMessages1).text(response);
 
 			// Clear the form.
 			// $('#name').val('');
@@ -158,24 +158,83 @@ $(function() {
 			// $('#diet').val('');
 			// $('#transport').prop('checked',false);
 			// $('#message').val('');
-			$("#ajax-rsvp").hide();
+			$("#ajax-rsvp1").hide();
 		})
 		.fail(function(data) {
 			// Make sure that the formMessages div has the 'error' class.
-			$(formMessages).removeClass('success');
-			$(formMessages).addClass('error');
+			$(formMessages1).removeClass('success');
+			$(formMessages1).addClass('error');
 
 			// Set the message text.
 			if (data.responseText !== '') {
-				$(formMessages).text(data.responseText);
+				$(formMessages1).text(data.responseText);
 			} else {
-				$(formMessages).text('An error occurred and your message could not be sent.');
+				$(formMessages1).text('An error occurred and your message could not be sent.');
 			}
 		});
 
 	});
 
 });
+
+// rsvp 2 form submit
+
+$(function() {
+
+	// Get the form.
+	var form = $('#ajax-rsvp2');
+
+	// Get the messages div.
+	var formMessages2 = $('#form-messages2');
+
+	// Set up an event listener for the contact form.
+	$(form).submit(function(e) {
+		// Stop the browser from submitting the form.
+		e.preventDefault();
+
+		// Serialize the form data.
+		var formData = $(form).serialize();
+
+		// Submit the form using AJAX.
+		$.ajax({
+			type: 'POST',
+			url: $(form).attr('action'),
+			data: formData
+		})
+		.done(function(response) {
+			// Make sure that the formMessages div has the 'success' class.
+			$(formMessages2).removeClass('error');
+			$(formMessages2).addClass('success');
+
+			// Set the message text.
+			$(formMessages2).text(response);
+
+			// Clear the form.
+			// $('#name').val('');
+			// $('#email').val('');
+			// $('#attend').prop('checked',false);
+			// $('#diet').val('');
+			// $('#transport').prop('checked',false);
+			// $('#message').val('');
+			$("#ajax-rsvp2").hide();
+		})
+		.fail(function(data) {
+			// Make sure that the formMessages div has the 'error' class.
+			$(formMessages2).removeClass('success');
+			$(formMessages2).addClass('error');
+
+			// Set the message text.
+			if (data.responseText !== '') {
+				$(formMessages2).text(data.responseText);
+			} else {
+				$(formMessages2).text('An error occurred and your message could not be sent.');
+			}
+		});
+
+	});
+
+});
+
 
 
 
